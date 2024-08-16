f1848803308deb588dce99711dc265bf6390c98c
huynhhonghuan <huynhhonghuan@gmail.com>
2024-08-16 12:36:24 +0700

    1

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -2,30 +2,4 @@ const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
 
-    // Thông tin người gửi
-    sender_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-
-    // Thông tin người nhận
-    receiver_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-
-    // Nội dung tin nhắn
-    content: {
-        type: String,
-        required: true
-    },
-
-    // Th��i gian gửi tin nhắn
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
-}, {
 });
